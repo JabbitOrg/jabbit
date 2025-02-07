fca438d67ae89e14ff9a234facf299cb21a692e4
ryanbae94 <ryanbae94@gmail.com>
2025-02-07 15:30:04 +0900

    feat: Home 화면에 네비게이션 적용

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
+import { Flex } from '@chakra-ui/react';
+import Navigation from './components/Navigation/Naviagtion';
+
 const Home = () => {
-  return <div>Jabbit</div>;
+  return (
+    <Flex flexDirection="column" width="100%" height="100vh" paddingTop="35px">
+      <Navigation />
+    </Flex>
+  );
 };
 
 export default Home;
