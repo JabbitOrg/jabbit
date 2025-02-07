0252f64da3d6e1279444cfd802f989ec4458d957
ryanbae94 <ryanbae94@gmail.com>
2025-02-07 20:40:21 +0900

    feat: footer 구현

---
+++ src/app/components/Footer/Footer.tsx
@@ -0,0 +1,19 @@
+import { Box, Flex, Text } from '@chakra-ui/react';
+import FooterLogoSVG from '@/public/assets/logo_footer.svg';
+const Footer = () => {
+  return (
+    <Box width="1920px" padding="52px 320px" borderTop="2px solid #f2f3f5">
+      <Flex flexDirection="column" gap="20px">
+        <FooterLogoSVG />
+        <Text fontSize="14px" color="#a3a3a3">
+          대표자명: 한승호 | Contact: jabbitmy@gmail.com
+        </Text>
+        <Text fontSize="14px" color="#a3a3a3">
+          서비스 이용 약관 개인정보 수집 동의
+        </Text>
+      </Flex>
+    </Box>
+  );
+};
+
+export default Footer;
