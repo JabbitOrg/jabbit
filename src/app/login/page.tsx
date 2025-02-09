bd92af598bf45ee4d1bbdbbd6656f8ec4f5dbc2d
Hoony0321 <eogns0321@gmail.com>
2025-02-09 00:07:43 -0800

    reformat : 로그인 페이지 OAUTH 상수 적용

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -6,17 +6,16 @@ import KakaoLoginBtnSVG from '@/public/assets/KakaoLoginBtn.svg';
 import NaverLoginBtnSVG from '@/public/assets/NaverLoginBtn.svg';
 import LogoSVG from '@/public/assets/logo.svg';
 import { Text } from '@chakra-ui/react';
-const naverState = uuidv4();
-const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI}&response_type=code`;
-const NAVER_AUTH_URL = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${process.env.NEXT_PUBLIC_NAVER_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_NAVER_REDIRECT_URI}&state=${naverState}`;
+import { OAUTH } from '@/src/app/constants/auth';
 
 const handleKakaoLogin = () => {
-  window.location.href = KAKAO_AUTH_URL;
+  window.location.href = OAUTH.KAKAO.AUTH_URL;
 };
 
 const handleNaverLogin = () => {
+  const naverState = uuidv4();
   localStorage.setItem('naverState', naverState);
-  window.location.href = NAVER_AUTH_URL;
+  window.location.href = OAUTH.NAVER.AUTH_URL(naverState);
 };
 
 const Login = () => {
