0ad95e86e666edbf7fe2160182a551bd175d66ca
Hoony0321 <eogns0321@gmail.com>
2025-02-09 11:01:25 -0800

    refactor : JWT 토큰 만료 기간 상수로 변경

--- src/app/constants/jwt.ts
+++ src/app/constants/jwt.ts
@@ -1,6 +1,8 @@
 import { Secret } from 'jsonwebtoken';
 
+export const JWT_EXPIRES_IN = '3d';
+
 export const JWT = {
   SECRET: process.env.JWT_SECRET as Secret,
-  EXPIRES_IN: '3d',
+  EXPIRES_IN: JWT_EXPIRES_IN,
 } as const;
