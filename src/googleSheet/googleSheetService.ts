1a8f4f7ab815cde1fda62e9730ae0d6091444b80
Hoony0321 <eogns0321@gmail.com>
2025-02-16 17:43:42 -0800

    config : google sheet api 사용을 위한 googleSheetService 추가

---
+++ src/googleSheet/googleSheetService.ts
@@ -0,0 +1,60 @@
+import { google } from 'googleapis';
+
+const auth = new google.auth.GoogleAuth({
+  credentials: {
+    client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+    private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+  },
+  scopes: ['https://www.googleapis.com/auth/spreadsheets'],
+});
+
+const sheetsClient = google.sheets({ version: 'v4', auth });
+const spreadsheetId = process.env.GOOGLE_SPREADSHEET_ID;
+
+export const readSheetData = async (sheetName: string, range: string) => {
+  const response = await sheetsClient.spreadsheets.values.get({
+    spreadsheetId,
+    range: `${sheetName}!${range}`,
+  });
+
+  return response.data.values;
+};
+
+export const appendSheetData = async (
+  sheetName: string,
+  range: string,
+  values: any[][],
+) => {
+  const response = await sheetsClient.spreadsheets.values.append({
+    spreadsheetId,
+    range: `${sheetName}!${range}`,
+    valueInputOption: 'RAW',
+    requestBody: { values },
+  });
+
+  return response.data;
+};
+
+export const updateSheetData = async (
+  sheetName: string,
+  range: string,
+  values: any[][],
+) => {
+  const response = await sheetsClient.spreadsheets.values.update({
+    spreadsheetId,
+    range: `${sheetName}!${range}`,
+    valueInputOption: 'RAW',
+    requestBody: { values },
+  });
+
+  return response.data;
+};
+
+export const deleteSheetData = async (sheetName: string, range: string) => {
+  const response = await sheetsClient.spreadsheets.values.clear({
+    spreadsheetId,
+    range: `${sheetName}!${range}`,
+  });
+
+  return response.data;
+};
