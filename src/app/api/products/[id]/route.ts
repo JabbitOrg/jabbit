7a8dad38b00a384c6d5a8c7d64cd4737116eadc6
Hoony0321 <eogns0321@gmail.com>
2025-02-17 16:23:47 -0800

    feat : 상품 목록 조회 / 상세 조회 API 추가

---
+++ src/app/api/products/[id]/route.ts
@@ -0,0 +1,73 @@
+import { API_MESSAGES } from '@/src/constants/API_MESSAGES';
+import { ERROR_INFOS } from '@/src/constants/ERROR_INFOS';
+import {
+  EXPERT_SHEET_NAME,
+  PRODUCT_SHEET_NAME,
+} from '@/src/constants/SHEET_INFOS';
+import { ProductDto } from '@/src/dtos/product.dto';
+import { findSheetDataById } from '@/src/googleSheet/googleSheetService';
+import { ExpertMapper } from '@/src/mappers/expert.mapper';
+import { ProductMapper } from '@/src/mappers/product.mapper';
+import {
+  createErrorApiResponse,
+  createSuccessApiResponse,
+} from '@/src/utils/apiResponseUtils';
+
+export async function GET(
+  request: Request,
+  { params }: { params: { id: string } },
+) {
+  const { id } = await params;
+  const productId = id;
+
+  const productRawData = await findSheetDataById(
+    PRODUCT_SHEET_NAME,
+    'A2:G',
+    productId,
+  );
+  if (!productRawData) {
+    console.error(`Product not found for id ${productId}:`, productRawData);
+    return createErrorApiResponse(
+      ERROR_INFOS['googleSheet.noData'].statusCode,
+      'googleSheet.noData',
+    );
+  }
+
+  const expertRawData = await findSheetDataById(
+    EXPERT_SHEET_NAME,
+    'A2:G',
+    productRawData[1],
+  );
+
+  if (!expertRawData) {
+    console.error(
+      `Expert not found for product with id ${productId}:`,
+      productRawData,
+    );
+    return createErrorApiResponse(
+      ERROR_INFOS['googleSheet.expertNotFound'].statusCode,
+      'googleSheet.expertNotFound',
+    );
+  }
+
+  try {
+    const parsedExpert = ExpertMapper.fromSheetRow(expertRawData);
+    const parsedProduct = ProductMapper.fromSheetRow(
+      productRawData,
+      parsedExpert,
+    );
+    const productDto = new ProductDto(parsedProduct);
+
+    return createSuccessApiResponse(
+      200,
+      productDto,
+      API_MESSAGES['READ_SUCCESS'],
+    );
+  } catch (error) {
+    console.error(`Parsing error for product with id ${productId}:`, error);
+    return createErrorApiResponse(
+      ERROR_INFOS['googleSheet.parseError'].statusCode,
+      'googleSheet.parseError',
+    );
+  }
+}
---
+++ src/app/api/products/route.ts
@@ -0,0 +1,58 @@
+import { EXPERT_SHEET_NAME } from '@/src/constants/SHEET_INFOS';
+import { readSheetData } from '@/src/googleSheet/googleSheetService';
+import { PRODUCT_SHEET_NAME } from '@/src/constants/SHEET_INFOS';
+import {
+  createErrorApiResponse,
+  createSuccessApiResponse,
+} from '@/src/utils/apiResponseUtils';
+import { ERROR_INFOS } from '@/src/constants/ERROR_INFOS';
+import { ProductMapper } from '@/src/mappers/product.mapper';
+import { ProductSimpleDto } from '@/src/dtos/product.simple.dto';
+import { ExpertMapper } from '@/src/mappers/expert.mapper';
+import { API_MESSAGES } from '@/src/constants/API_MESSAGES';
+
+export async function GET() {
+  const productsRawData = await readSheetData(PRODUCT_SHEET_NAME, 'A2:G');
+  if (!productsRawData) {
+    return createErrorApiResponse(
+      ERROR_INFOS['googleSheet.noData'].statusCode,
+      'googleSheet.noData',
+    );
+  }
+
+  const expertRawData = await readSheetData(EXPERT_SHEET_NAME, 'A2:G');
+  if (!expertRawData) {
+    return createErrorApiResponse(
+      ERROR_INFOS['googleSheet.noData'].statusCode,
+      'googleSheet.noData',
+    );
+  }
+
+  const productSimpleDtos: ProductSimpleDto[] = [];
+
+  for (const product of productsRawData) {
+    try {
+      const expert = expertRawData.find((expert) => expert[0] === product[1]);
+      if (!expert) {
+        console.error(
+          `Expert not found for product id ${product[0]}:`,
+          product,
+        );
+        continue;
+      }
+      const parsedProduct = ProductMapper.fromSheetRow(
+        product,
+        ExpertMapper.fromSheetRow(expert),
+      );
+      productSimpleDtos.push(new ProductSimpleDto(parsedProduct));
+    } catch (error) {
+      console.error(`Parsing error on product id ${product[0]}:`, error);
+    }
+  }
+
+  return createSuccessApiResponse(
+    200,
+    productSimpleDtos,
+    API_MESSAGES['READ_SUCCESS'],
+  );
+}
