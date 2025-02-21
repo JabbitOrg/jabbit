f4ffac559aab6e3cf3d8b8f4a61bd72f6b5741e6
Daehoon Go <50730897+Hoony0321@users.noreply.github.com>
2025-02-21 15:02:58 -0800

    전문가 리스트 페이지 작업 (#27)
    * config : 인증마크 SVG 마크 추가
    
    * chore : test용 profile png 추가
    
    * config : 전문가 주요 분야 상수 추가
    
    * feat : 전문가 목록 페이지 좌측 카테고리바 컴포넌트 추가
    
    * feat : 전문가 목록 리스트 컴포넌트 및 카드 컴포넌트 추가
    
    * feat : 전문가 목록 페이지 상단 타이틀 컴포넌트 추가
    
    * feat : 상품 최소가격 추출 함수 추가
    
    * feat : 전문가 목록 페이지 추가
    
    * feat : 네비게이션바에 전문가 목록 링크 추가
    
    * chore : Profile 이미지 왼쪽으로 밀리는 부분 수정
    
    * feat : 홈페이지에서 카테고리 버튼 누르면 해당 카테고리에 맞게 experts 페이지 라우팅 기능 추가
    
    * feat : LeftSideBar에서 Selected된 부분만 바탕색 적용되게 수정
    
    * fix : useSearchParam Suspense로 안 감싸줘서 발생한 에러 해결
    
    Content 컴포넌트 생성해서 해당 부분에 모든 컴포넌트 넣음.

---
+++ src/app/experts/components/Content.tsx
@@ -0,0 +1,107 @@
+import { Box } from '@chakra-ui/react';
+import { Flex } from '@chakra-ui/react';
+import Navigation from '../../components/Navigation/Naviagtion';
+import PageTitle from './PageTitle';
+import LeftSideBar from './LeftSideBar';
+import ProductInfoList from './ProductInfoList';
+import Footer from '../../components/Footer/Footer';
+import { ProductSimpleDto } from '@/src/server/dtos/product.simple.dto';
+import { useSearchParams } from 'next/navigation';
+import { useState, useEffect } from 'react';
+import { AppError } from '@/src/errors/AppError';
+import { ERROR_INFOS } from '@/src/constants/ERROR_INFOS';
+import { useErrorToast } from '@/src/errors/useErrorToast';
+
+const Content = () => {
+  const searchParams = useSearchParams();
+  const specialty = searchParams.get('specialty');
+
+  const [selectedSpecialty, setSelectedSpecialty] = useState<string>(
+    specialty || '종합재무상담',
+  );
+  const [products, setProducts] = useState<ProductSimpleDto[]>([]);
+  const [filteredProducts, setFilteredProducts] = useState<ProductSimpleDto[]>(
+    [],
+  );
+  const [isLoading, setIsLoading] = useState(true);
+  const { showErrorToast } = useErrorToast();
+
+  const fetchProducts = async () => {
+    setIsLoading(true);
+    fetch('http://localhost:3000/api/products')
+      .then(async (res) => {
+        const resData = await res.json();
+        if (res.status != 200 || !resData.success) {
+          throw new AppError({
+            statusCode: ERROR_INFOS['fetchFailed'].statusCode,
+            errorInfoKey: 'fetchFailed',
+          });
+        }
+
+        const productDtos = resData.data.map(
+          (product: any) => new ProductSimpleDto(product),
+        );
+        setProducts(productDtos);
+      })
+      .catch((error) => {
+        const isAppError = error instanceof AppError;
+
+        if (!isAppError) {
+          error = new AppError({
+            statusCode: ERROR_INFOS['fetchFailed'].statusCode,
+            errorInfoKey: 'fetchFailed',
+          });
+        }
+
+        showErrorToast(error);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  useEffect(() => {
+    if (products.length > 0) {
+      const filteredProducts = products.filter((product) =>
+        product.expert.specialties.includes(selectedSpecialty),
+      );
+      setFilteredProducts(filteredProducts);
+    }
+  }, [products, selectedSpecialty]);
+
+  useEffect(() => {
+    if (specialty) {
+      setSelectedSpecialty(specialty);
+    }
+  }, [specialty]);
+  return (
+    <Flex
+      flexDirection="column"
+      width="100%"
+      minHeight="100vh"
+      paddingTop="35px"
+      alignItems="center"
+    >
+      <Navigation />
+      <PageTitle />
+      <Box h="122px" />
+
+      <Flex width="100%" maxWidth="1280px" flex="1" position="relative">
+        <LeftSideBar
+          selectedSpecialty={selectedSpecialty}
+          setSelectedSpecialty={setSelectedSpecialty}
+        />
+
+        <ProductInfoList products={filteredProducts} isLoading={isLoading} />
+      </Flex>
+      <Box h="122px" />
+      <Footer />
+    </Flex>
+  );
+};
+
+export default Content;
---
+++ src/app/experts/components/PageTitle.tsx
@@ -0,0 +1,13 @@
+import { Text, Flex } from '@chakra-ui/react';
+
+const PageTitle = () => {
+  return (
+    <Flex w="100%" maxWidth="1280px" mt="94px">
+      <Text fontSize="38px" fontWeight="600" color="main.black_1">
+        전문가 찾기
+      </Text>
+    </Flex>
+  );
+};
+
+export default PageTitle;
