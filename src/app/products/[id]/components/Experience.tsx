5ebb364e2c157b7b1b59a0344acef94b692e477f
ryanbae94 <ryanbae94@gmail.com>
2025-02-22 09:58:46 +0900

    feat: 경력 사항 컴포넌트 구현

---
+++ src/app/products/[id]/components/Experience.tsx
@@ -0,0 +1,21 @@
+import { Flex, Text } from '@chakra-ui/react';
+import CurrentExperienceSVG from '@/public/assets/current_experience_badge.svg';
+import FormerExperienceSVG from '@/public/assets/former_experience_badge.svg';
+
+interface ExperienceProps {
+  type: '현' | '전';
+  description: string;
+}
+
+const Experience = ({ type, description }: ExperienceProps) => {
+  return (
+    <Flex gap="10px" alignItems="center">
+      {type === '현' ? <CurrentExperienceSVG /> : <FormerExperienceSVG />}
+      <Text fontSize="16px" color="main.black_4">
+        {description}
+      </Text>
+    </Flex>
+  );
+};
+
+export default Experience;
