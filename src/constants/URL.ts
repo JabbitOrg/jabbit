90a148086c8aa23f59d3081e29d8ac4abeb1a7eb
Hoony0321 <eogns0321@gmail.com>
2025-02-25 12:17:14 -0800

    config : URL constants 생성

---
+++ src/constants/URL.ts
@@ -0,0 +1,5 @@
+export const PRIVACY_POLICY_URL =
+  'https://ludicrous-icebreaker-097.notion.site/19f771a76bb5807db68ae6f5ad2c369f?pvs=4';
+
+export const TERMS_OF_SERVICE_URL =
+  'https://ludicrous-icebreaker-097.notion.site/19f771a76bb580c8a74acaa9080bc086?pvs=4';
