088d50268b0940e73d841048509716a829bc2b52
Hoony0321 <eogns0321@gmail.com>
2025-02-25 13:03:11 -0800

    feat : 채널톡 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Theme } from '@chakra-ui/react';
 import localFont from 'next/font/local';
 import { Toaster } from '@/src/components/ui/toaster';
 import { GoogleAnalytics, GoogleTagManager } from '@next/third-parties/google';
+import ChannelTalk from './common/ChannelTalk/ChannelTalk';
 export const metadata: Metadata = {
   title: 'JABBIT (재빗)',
   description: '재무관리의 모든 것',
@@ -22,6 +23,7 @@ export default function RootLayout({
   return (
     <html suppressHydrationWarning className={pretendard.className}>
       <body>
+        <ChannelTalk />
         <Provider>
           <Theme appearance="light">{children}</Theme>
           <Toaster />
