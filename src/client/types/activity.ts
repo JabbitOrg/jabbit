02a61675a1378802751b61572f9a6e06b5e5397e
Hoony0321 <eogns0321@gmail.com>
2025-03-01 18:27:31 -0800

    feat : expert activity에 url 칼럼이 포함이 될 수 있도록 수정

--- src/client/types/activity.ts
+++ src/client/types/activity.ts
@@ -2,4 +2,5 @@ export type activity = {
   type: '교육/강연' | '보도/방송';
   year: number;
   description: string;
+  url?: string;
 };
--- src/client/utils/parser.ts
+++ src/client/utils/parser.ts
@@ -4,17 +4,25 @@ export const parseDescription = (description: string) => {
   return description.split('\n').map((line) => line);
 };
 
+interface PressActivity {
+  string: string;
+  url?: string;
+}
+
 export const parseActivity = (activities: activity[]) => {
   const parsedActivity = {
     education: [] as string[],
-    press: [] as string[],
+    press: [] as PressActivity[],
   };
 
   activities.map((activity) => {
     if (activity.type === '교육/강연')
       parsedActivity.education.push(`${activity.year} ${activity.description}`);
     else if (activity.type === '보도/방송')
-      parsedActivity.press.push(`${activity.year} ${activity.description}`);
+      parsedActivity.press.push({
+        string: `${activity.year} ${activity.description}`,
+        url: activity.url,
+      });
   });
 
   parsedActivity.education.sort((a, b) => {
@@ -24,8 +32,8 @@ export const parseActivity = (activities: activity[]) => {
   });
 
   parsedActivity.press.sort((a, b) => {
-    const yearA = a.split(' ')[0];
-    const yearB = b.split(' ')[0];
+    const yearA = a.string.split(' ')[0];
+    const yearB = b.string.split(' ')[0];
     return parseInt(yearB) - parseInt(yearA);
   });
 
