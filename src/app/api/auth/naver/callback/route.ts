be04d448e40e000df4a6fc6ac4caee9eb43dee31
Hoony0321 <eogns0321@gmail.com>
2025-03-19 12:17:55 -0700

    feat : callback page에서 user email도 받도록 수정

--- src/app/api/auth/naver/callback/route.ts
+++ src/app/api/auth/naver/callback/route.ts
@@ -50,6 +50,7 @@ export async function GET(req: Request) {
         body: JSON.stringify({
           id: userData.response.id,
           provider: 'NAVER',
+          email: userData.response.email,
           createdAt: new Date(new Date().getTime() + 9 * 60 * 60 * 1000),
         }),
       });
