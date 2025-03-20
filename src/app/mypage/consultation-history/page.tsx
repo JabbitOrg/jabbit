7a545c62d5476ccdb4fe2410ada2fc5716340d3d
ryanbae94 <ryanbae94@gmail.com>
2025-03-21 03:15:16 +0900

    feat: 마이페이지 내역이 없을 때 보여주는 컴포넌트 구현

--- src/app/mypage/consultation-history/page.tsx
+++ src/app/mypage/consultation-history/page.tsx
@@ -1,6 +1,7 @@
 import { Flex } from '@chakra-ui/react';
 import ConsultationCard from '../components/ConsultationCard';
 import { BASE_URL } from '@/src/client/constants/API';
+import NoData from '../components/NoData';
 
 const ConsultationHistory = async ({
   searchParams,
@@ -17,6 +18,10 @@ const ConsultationHistory = async ({
 
   const consultationHistoryData = await response.json();
   const data = consultationHistoryData.data;
+
+  if (!data || data.length === 0) {
+    return <NoData page="consultation-history" />;
+  }
 
   return (
     <Flex flexDirection="column" gap="64px" w="100%">
