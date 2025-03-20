0385d7eab1983695de232c3885d485e455d54411
ryanbae94 <ryanbae94@gmail.com>
2025-03-20 23:07:01 +0900

    chore: 마이페이지 카테고리 목록화

---
+++ src/client/constants/MYPAGE.ts
@@ -0,0 +1,23 @@
+// 정식 오픈 시 사용될 데이터
+// export const MYPAGE_SIDEBAR_DATA = [
+//   {
+//     title: '내 정보',
+//     slug: 'my',
+//   },
+//   {
+//     title: '재무 분석',
+//     slug: 'analysis',
+//   },
+//   {
+//     title: '상담 내역',
+//     slug: 'consultation-history',
+//   },
+// ];
+
+// 임시 데이터
+export const MYPAGE_SIDEBAR_DATA = [
+  {
+    title: '상담 내역',
+    slug: 'consultation-history',
+  },
+];
