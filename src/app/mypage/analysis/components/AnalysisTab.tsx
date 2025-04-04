00bea8ad52e213cf856993e61ee78cc95abd4aeb
ryanbae94 <ryanbae94@gmail.com>
2025-04-04 18:31:01 +0900

    feat: 상단탭 구현

---
+++ src/app/mypage/analysis/components/AnalysisTab.tsx
@@ -0,0 +1,77 @@
+'use client';
+import { ANALYSIS_TAB_DATA } from '@/src/client/constants/MYPAGE';
+import { Flex } from '@chakra-ui/react';
+import { useRouter, useSearchParams } from 'next/navigation';
+
+interface AnalysisTabProps {
+  defaultTab?: string;
+}
+
+const TabItem = ({
+  tab,
+  isSelected,
+  onClick,
+}: {
+  tab: string;
+  isSelected: boolean;
+  onClick: (tab: string) => void;
+}) => {
+  return (
+    <Flex
+      justifyContent="center"
+      alignItems="center"
+      cursor="pointer"
+      onClick={() => onClick(tab)}
+      position="relative"
+      pb="2px"
+      fontSize="18px"
+      fontWeight={isSelected ? '600' : '500'}
+      color={isSelected ? 'primary' : 'main.black_4'}
+    >
+      {tab}
+      {isSelected && (
+        <Flex
+          position="absolute"
+          bottom="-2px"
+          left="0"
+          right="0"
+          height="2px"
+          bg="primary"
+          zIndex="1"
+        />
+      )}
+    </Flex>
+  );
+};
+
+const AnalysisTab = ({ defaultTab }: AnalysisTabProps) => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const currentTab = searchParams.get('tab') || defaultTab;
+
+  const handleTab = (tab: string) => {
+    const params = new URLSearchParams(searchParams.toString());
+    params.set('tab', tab);
+    router.push(`?${params.toString()}`);
+  };
+
+  return (
+    <Flex
+      width="100%"
+      gap="40px"
+      borderBottom="2px solid var(--chakra-colors-main-line)"
+      position="relative"
+    >
+      {ANALYSIS_TAB_DATA.map((tab) => (
+        <TabItem
+          key={tab}
+          tab={tab}
+          isSelected={tab === currentTab}
+          onClick={handleTab}
+        />
+      ))}
+    </Flex>
+  );
+};
+
+export default AnalysisTab;
--- src/app/mypage/analysis/page.tsx
+++ src/app/mypage/analysis/page.tsx
@@ -1,5 +1,13 @@
+import { Flex } from '@chakra-ui/react';
+import AnalysisTab from './components/AnalysisTab';
+import { ANALYSIS_TAB_DATA } from '@/src/client/constants/MYPAGE';
+
 const Analysis = () => {
-  return <div>Analysis</div>;
+  return (
+    <Flex flexDirection="column" gap="64px" w="100%">
+      <AnalysisTab defaultTab={ANALYSIS_TAB_DATA[0]} />
+    </Flex>
+  );
 };
 
 export default Analysis;
