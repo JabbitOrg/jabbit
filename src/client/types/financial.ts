281f2497f25a1a75768cf66a29de3311bfe6f912
Hoony0321 <eogns0321@gmail.com>
2025-04-05 10:45:01 -0700

    feat : financial type & util function 추가

---
+++ src/client/types/financial.ts
@@ -0,0 +1,28 @@
+export type EvaluatedResult = {
+    name: string;
+    label: string;
+    evaluation: boolean;
+    myRatio: number;
+}
+
+export type RatioConditionType = 'min' | 'max' | 'range';
+
+type BaseRatioConfig = {
+  label: string;
+  condition: RatioConditionType;
+  displayText: string;
+};
+
+type MinOrMaxConfig = BaseRatioConfig & {
+  condition: 'min' | 'max';
+  value: number;
+};
+
+type RangeConfig = BaseRatioConfig & {
+  condition: 'range';
+  value: number[];
+};
+
+export type FinancialRatioItem = MinOrMaxConfig | RangeConfig;
+
+export type FinancialRatioConfig = Record<string, FinancialRatioItem>;
