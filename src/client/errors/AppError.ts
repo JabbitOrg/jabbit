f374a492ac48db403a776873e0ecda586c14cc4a
Hoony0321 <eogns0321@gmail.com>
2025-04-22 18:05:49 -0700

    refactor : AppError 구조 변경 & useErrorToast 구조 간단화

--- src/client/errors/AppError.ts
+++ src/client/errors/AppError.ts
@@ -1,18 +1,7 @@
-import { ERROR_INFOS } from '../constants/ERROR_INFOS';
-
 export class AppError extends Error {
-  statusCode?: number;
-  errorInfoKey?: keyof typeof ERROR_INFOS;
-
-  constructor({
-    statusCode,
-    errorInfoKey,
-  }: {
-    statusCode?: number;
-    errorInfoKey?: keyof typeof ERROR_INFOS;
-  }) {
-    super(ERROR_INFOS[errorInfoKey ?? 'unknownError'].message);
-    this.statusCode = statusCode;
-    this.errorInfoKey = errorInfoKey;
+  name: string;
+  constructor({ name, message }: { name: string; message: string }) {
+    super(message || '알 수 없는 오류');
+    this.name = name || 'UNKNOWN_ERROR';
   }
 }
