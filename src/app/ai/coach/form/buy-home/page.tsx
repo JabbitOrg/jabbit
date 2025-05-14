059afa6bb61aa3364368be37a26eeb9311e80f44
MiJung Lee <mijunglee1215@gmail.com>
2025-05-14 18:19:47 +0900

    feat: 설문 응답값 요약 페이지 이동 로직 추가

--- src/app/ai/coach/form/buy-home/page.tsx
+++ src/app/ai/coach/form/buy-home/page.tsx
@@ -1,18 +1,32 @@
 'use client';
 
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
 import { Box, Flex, Stack } from '@chakra-ui/react';
 import ProgressBar from '../../_components/form/ProgressBar';
-import { useState } from 'react';
 import StepController from '../../_components/form/StepController';
 import Step from '../../_components/form/Step';
 import Question from '../../_components/form/Question';
 import Answer from '../../_components/form/Answer';
 import SurveyQuestions from '../../../../data/buy-home-survey.json';
+import { useSurveyStore } from '../../../../../client/store/surveyStore';
 
 const totalStep = 8;
 
 function BuyHomeFormPage() {
+  const router = useRouter();
   const [currentStep, setCurrentStep] = useState(1);
+  const { setAnswer, answers } = useSurveyStore();
+  console.log('buyanswers', answers);
+
+  const handleAnswer = (answer: string | number) => {
+    setAnswer(currentStep, answer);
+
+    // If this is the last question, navigate to summary
+    if (currentStep === totalStep) {
+      router.push('/ai/coach/form/buy-home/summary');
+    }
+  };
 
   const renderStepContent = () => {
     const currentQuestion = SurveyQuestions.find((q) => q.id === currentStep);
@@ -29,6 +43,9 @@ function BuyHomeFormPage() {
             type as 'input-year' | 'input-area' | 'choice-full' | 'choice-grid'
           }
           answerChoices={answerChoices}
+          onAnswerSelect={handleAnswer}
+          selectedAnswers={answers}
+          currentStep={currentStep}
         />
       </Stack>
     );
@@ -46,8 +63,6 @@ function BuyHomeFormPage() {
             onNextStep={() => setCurrentStep(currentStep + 1)}
           />
         </Flex>
-
-        {/* step content */}
         {renderStepContent()}
       </Stack>
     </Box>
