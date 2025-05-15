0b042057738e9d261e0dc29fc70f5846b4041ce5
이미정 <mijunglee1215@gmail.com>
2025-05-15 11:01:35 +0900

    feat: 설문 응답값 스토어 두개로 분리

--- src/client/store/survey/financialGoalSurveyStore.ts
+++ src/client/store/survey/financialGoalSurveyStore.ts
@@ -6,13 +6,13 @@ interface answer {
   text: string;
 }
 
-interface SurveyState {
+interface financialGoalSurveyState {
   answers: Record<number, answer>;
   setAnswer: (id: number, answer: string | number, text: string) => void;
   clearAnswers: () => void;
 }
 
-export const useSurveyStore = create<SurveyState>((set) => ({
+export const usefinancialGoalSurveyStore = create<financialGoalSurveyState>((set) => ({
   answers: {},
   setAnswer: (id, answer, text) =>
     set((state) => ({
