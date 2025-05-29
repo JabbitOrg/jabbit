99638c65354a97d181b6d0be15f700bc70703a4b
MiJung Lee <mijunglee1215@gmail.com>
2025-05-29 19:20:43 +0900

    fix: 1인 가구 선택 시에 신혼부부 텍스트 문구 나오는 이슈 해결

--- src/app/ai/coach/_components/form/Question.tsx
+++ src/app/ai/coach/_components/form/Question.tsx
@@ -1,11 +1,17 @@
 import { Stack, Text } from '@chakra-ui/react';
+import { useBuyHomeSurveyStore } from '@/src/app/ai/coach/_store/buyHomeSurveyStore';
 
 interface QuestionProps {
   title: string;
   description: string | null;
+  id?: number;
 }
 
-function Question({ title, description }: QuestionProps) {
+function Question({ title, description, id }: QuestionProps) {
+  const { response } = useBuyHomeSurveyStore();
+  const householdType =
+    response['q3'] === '결혼해서 신혼부부에요' ? '초혼 신혼부부' : '1인 가구';
+  console.log('familyType', householdType);
   return (
     <Stack direction="column" gap="12px" height="98px">
       <Text
@@ -13,11 +19,11 @@ function Question({ title, description }: QuestionProps) {
         color="font.900"
         dangerouslySetInnerHTML={{ __html: title }}
       />
-      {description && (
+      {description && id && id >= 4 && (
         <Text
           textStyle="mobile_b1_semi"
           color="font.700"
-          dangerouslySetInnerHTML={{ __html: description }}
+          dangerouslySetInnerHTML={{ __html: householdType + description }}
         />
       )}
     </Stack>
