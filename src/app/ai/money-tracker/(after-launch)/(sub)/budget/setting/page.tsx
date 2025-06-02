85ad660d8eeb201b92892ce790c7e9d19f723d10
rachel490 <ccori12@naver.com>
2025-06-02 20:32:03 +0900

    feat: 예산 설정 페이지 추가

--- src/app/ai/money-tracker/(after-launch)/(sub)/budget/setting/page.tsx
+++ src/app/ai/money-tracker/(after-launch)/(sub)/budget/setting/page.tsx
@@ -1,5 +1,29 @@
+import { Stack, Text } from '@chakra-ui/react';
+import { EXPENSE_CATEGORY_MAP } from '@/src/app/ai/money-tracker/_ constants/category';
+import Input from '@/src/app/components/Input/Input';
+
 function BudgetSettingPage() {
-  return <div>budget setting</div>;
+  return (
+    <Stack gap="24px" p="20px" pt="10px">
+      {EXPENSE_CATEGORY_MAP.map((item) => (
+        <Stack key={item.name} gap="8px">
+          <Text textStyle="mobile_b2">{`${item.icon} ${item.name}`}</Text>
+          <Input
+            placeholder="금액을 입력해주세요"
+            _placeholder={{
+              color: 'font.600',
+            }}
+            textStyle="mobile_b2"
+            bgColor="blue.100"
+            borderColor="font.400"
+            h="auto"
+            px="16px"
+            py="14px"
+          />
+        </Stack>
+      ))}
+    </Stack>
+  );
 }
 
 export default BudgetSettingPage;
