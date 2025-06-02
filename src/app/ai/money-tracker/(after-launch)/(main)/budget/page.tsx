df76dd2e1cf2bc9babe4a335863929e832eff44d
rachel490 <ccori12@naver.com>
2025-06-02 20:26:15 +0900

    feat: 현황탭 플러스 버튼추가

--- src/app/ai/money-tracker/(after-launch)/(main)/budget/page.tsx
+++ src/app/ai/money-tracker/(after-launch)/(main)/budget/page.tsx
@@ -1,13 +1,17 @@
 'use client';
 
 import { useState } from 'react';
-import { Stack } from '@chakra-ui/react';
+import { useRouter } from 'next/navigation';
+import { Flex, Stack } from '@chakra-ui/react';
+import PlusSvg from '@/src/client/assets/plus.svg';
 import GuideLinkButton from '@/src/app/ai/_components/GuideLinkButton';
 import { GUIDE_BUTTONS } from '@/src/app/ai/_constants/guide';
+import { IDENTIFIER_TO_PATH_MAP } from '@/src/app/ai/_constants/routes';
 import BudgetUnset from './_components/BudgetUnset';
 import BudgetStatus from './_components/BudgetStatus';
 
 function BudgetPage() {
+  const router = useRouter();
   const [isBudgetSet, setIsBudgetSet] = useState(false);
 
   console.log(setIsBudgetSet);
@@ -17,6 +21,21 @@ function BudgetPage() {
       <GuideLinkButton data={GUIDE_BUTTONS['money-tracker']} />
 
       {isBudgetSet ? <BudgetUnset /> : <BudgetStatus />}
+
+      <Flex
+        borderRadius="10px"
+        justifyContent="center"
+        alignItems="center"
+        cursor="pointer"
+        position="fixed"
+        bottom="88px"
+        right="0px"
+        onClick={() =>
+          router.push(IDENTIFIER_TO_PATH_MAP.MONEY_TRACKER_BUDGET_SETTING)
+        }
+      >
+        <PlusSvg />
+      </Flex>
     </Stack>
   );
 }
