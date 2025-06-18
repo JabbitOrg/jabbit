dc1b4567c4b78f39c6f473655354b3569d09971c
MiJung Lee <mijunglee1215@gmail.com>
2025-06-18 22:05:56 +0900

    fix: 401 오류에 따른 무한 루프 문제 해결

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,23 +3,10 @@ import { Suspense } from 'react';
 import { Spinner } from '@chakra-ui/react';
 import Login from './Login';
 import { usePwaStore } from '@/src/client/store/pwaStore';
-import { useAuthStore } from '@/src/client/store/authStore';
 import AiLogin from '@/src/app/ai/login/page';
-import { useEffect } from 'react';
-import { deleteJwtToken } from '@/src/client/lib/api/deleteJwt';
 
 function LoginPage() {
   const isPwa = usePwaStore((state) => state.isPwa);
-  const { accessToken } = useAuthStore();
-
-  useEffect(() => {
-    if (!accessToken) return;
-  
-    (async () => {
-      await deleteJwtToken();
-    })();
-  }, [accessToken]);
-  
 
   if (isPwa) {
     return <AiLogin />;
